04cb569da4a0050eaa912637534c3f6acf2c655d
Kari Oikarinen <kluge@kluge.fi>
2016-01-29 23:07:59 +0200

    Start own layer with ISO date insertion function
    Bind it to <f5> (inspired by Notepad).

---
+++ layers/kluge/funcs.el
@@ -0,0 +1,6 @@
+(defun kluge-insert-date (arg)
+  (interactive "P")
+  (insert (cond
+           ((not arg) (format-time-string "%Y-%m-%d"))
+           ((equal arg '(4)) (format-time-string "%Y-%m-%dT%H:%M"))
+           ((equal arg '(16)) (format-time-string "%Y-%m-%d %H:%M")))))
---
+++ layers/kluge/packages.el
@@ -0,0 +1,1 @@
+(setq kluge-packages '())
