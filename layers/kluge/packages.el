58e60c7d3a2e3d06b8bebd851442696a2f6f9861
Kari Oikarinen <kluge@kluge.fi>
2016-02-06 23:35:52 +0200

    Port old org configuration

--- layers/kluge/packages.el
+++ layers/kluge/packages.el
@@ -1,7 +1,8 @@
 (setq kluge-packages '(avy
                        cc-mode
                        dired+
-                       evil))
+                       evil
+                       org))
 
 (defun kluge/post-init-avy ()
   (define-key evil-motion-state-map (kbd "g SPC") 'avy-goto-char))
@@ -25,3 +26,35 @@
   ;; Don't override M-., it is useful. Rotate the repeat ring with C-, instead.
   (define-key evil-normal-state-map (kbd "M-.") nil)
   (define-key evil-normal-state-map (kbd "C-,") 'evil-repeat-pop-next))
+
+(defun kluge/post-init-org ()
+  (setq org-agenda-files '("~/org"))
+  (setq org-default-notes-file "~/org/inbox.org")
+  (setq org-refile-targets '((nil . (:maxlevel . 3))
+                             (org-agenda-files . (:maxlevel . 1))))
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t!)" "|" "DONE(d!)" "CANCELED(c@)")))
+  ;; Log date when task state is changed
+  (setq org-log-into-drawer t)
+
+  (global-set-key (kbd "C-c a") 'org-agenda)
+  (global-set-key (kbd "C-c b") 'org-iswitchb)
+  (global-set-key (kbd "C-c l") 'org-store-link)
+  (global-set-key (kbd "C-c c") 'org-capture)
+
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file "~/org/inbox.org")
+           "* TODO %?\n%U\n%i")
+          ("l" "Todo with link" entry (file "~/org/inbox.org")
+           "* TODO %?\n%U\n%i\n%a")
+          ("n" "Note" entry (file "~/org/inbox.org")
+           "* %?\n%U\n%i\n")
+          ("m" "Note with link" entry (file "~/org/inbox.org")
+           "* %?\n%U\n%i\n%a")
+          ("j" "Journal" entry (file+datetree "~/journal/2015.org")
+           "* %U\n%?")))
+
+  ;; Start in insert state in capture mode
+  (add-hook 'org-capture-mode-hook 'evil-insert-state)
+  )
