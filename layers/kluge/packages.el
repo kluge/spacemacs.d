9330277418a8b95f3f619b03997c7cc1fec4ae20
Kari Oikarinen <kluge@kluge.fi>
2016-02-06 22:31:18 +0200

    Dired+ and C++ style adjustments
    Also a keybinding for avy-goto-char.

--- layers/kluge/packages.el
+++ layers/kluge/packages.el
@@ -1,1 +1,20 @@
-(setq kluge-packages '())
+(setq kluge-packages '(avy
+                       cc-mode
+                       dired+))
+
+(defun kluge/post-init-avy ()
+  (define-key evil-motion-state-map (kbd "g SPC") 'avy-goto-char))
+
+(defun kluge/post-init-cc-mode ()
+   (c-add-style "kluge"
+                '("stroustrup"
+                  (c-offsets-alist
+                   (innamespace . 0))))
+   (setq c-default-style "kluge"))
+
+(defun kluge/init-dired+ ()
+  (use-package dired+
+    :init
+    (setq-default dired-listing-switches "-alh")
+    (setq dired-recursive-copies 'always) ; Don't prompt for copying directories
+    (define-key dired-mode-map (kbd "-") 'dired-up-directory)))
