2644de328b4dac3ce9f56f454181bcf51adb4f07
Kari Oikarinen <kluge@kluge.fi>
2016-02-06 22:41:38 +0200

    Leave M-. blank in evil

--- layers/kluge/packages.el
+++ layers/kluge/packages.el
@@ -1,6 +1,7 @@
 (setq kluge-packages '(avy
                        cc-mode
-                       dired+))
+                       dired+
+                       evil))
 
 (defun kluge/post-init-avy ()
   (define-key evil-motion-state-map (kbd "g SPC") 'avy-goto-char))
@@ -17,4 +18,10 @@
     :init
     (setq-default dired-listing-switches "-alh")
     (setq dired-recursive-copies 'always) ; Don't prompt for copying directories
+    :config
     (define-key dired-mode-map (kbd "-") 'dired-up-directory)))
+
+(defun kluge/post-init-evil ()
+  ;; Don't override M-., it is useful. Rotate the repeat ring with C-, instead.
+  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (define-key evil-normal-state-map (kbd "C-,") 'evil-repeat-pop-next))
