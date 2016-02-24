43bb864732f317286dd06b68249cb3976a95dc87
Kari Oikarinen <kluge@kluge.fi>
2016-02-24 10:55:08 +0200

    Exclude org-bullets to use regular ASCII bullets

--- init.el
+++ init.el
@@ -55,8 +55,9 @@ values."
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '(vi-tilde-fringe
-                                    evil-search-highlight-persist)
+   dotspacemacs-excluded-packages '(evil-search-highlight-persist
+                                    org-bullets
+                                    vi-tilde-fringe)
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
    ;; the list `dotspacemacs-configuration-layers'. (default t)
